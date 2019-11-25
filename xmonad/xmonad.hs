4abcb2bbee9d2bc9fcf377341daed36e07fca9b0
EntilZha <pedro@entilzha.io>
2019-11-25 09:00:32 +0100

    Update

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,6 +8,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
+import XMonad.Actions.NoBorders
 
 
 solarized :: Map String String
@@ -29,24 +30,32 @@ solarized = Map.fromList [
     ("cyan",     "#2aa198"),
     ("green", "#859900")]
 
+myManageHook = composeAll
+  [ className =? "discord" --> doShift "5:slack"
+  , className =? "slack" --> doShift "5:slack"
+  , className =? "slack-dpi" --> doShift "5:slack"
+  , className =? "code" --> doShift "2:code"
+  , manageDocks
+  ]
+
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ def
-    { terminal = "gnome-terminal"
+    { terminal = "kitty"
     , modMask = mod4Mask
-    , manageHook = manageDocks <+> manageHook def
+    , manageHook = myManageHook <+> manageHook def
     , layoutHook = avoidStruts $ layoutHook def
     , handleEventHook = handleEventHook def <+> docksEventHook
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppCurrent = xmobarColor (Map.findWithDefault "" "orange" solarized) "" . wrap "[" "]"
       , ppUrgent = xmobarColor (Map.findWithDefault "" "yellow" solarized) "" . wrap "*" "*" . xmobarStrip
-      , ppSep = "  ——  "
-      , ppTitle = xmobarColor (Map.findWithDefault "" "blue" solarized) ""
+      , ppSep = " —— "
+      , ppTitle = xmobarColor (Map.findWithDefault "" "blue" solarized) "" . shorten 50
       }
     , startupHook = setWMName "LG3D"
     , focusedBorderColor = "#236ea3"
-    , workspaces = ["1:term", "2:code", "3:web", "4:msg", "5:music", "6", "7", "8", "9"]
+    , workspaces = ["1:term", "2:code", "3:web", "4:msg", "5:slack", "6", "7", "8", "9"]
     } `additionalKeysP` myKeys
 
 
@@ -54,5 +63,11 @@ myKeys =
     [ (mask ++ "M-" ++ [key], screenWorkspace scr >>= flip whenJust (windows . action))
          | (key, scr)  <- zip "wer" [1,0,2] -- was [0..] *** change to match your screen order ***
          , (action, mask) <- [ (W.view, "") , (W.shift, "S-")]
+    ] ++ [
+      ("M-p", spawn "rofi -combi-modi window,drun,ssh -show combi -modi combi -terminal kitty"),
+      ("M-S-l", spawn "xscreensaver-command -lock"),
+      ("M-b", sendMessage ToggleStruts),
+      ("M-S-b", withFocused toggleBorder)
     ]
+
 
